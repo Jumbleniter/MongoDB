0bd4df958505b4b59e5f3e0a77d1dfd576befc9e
jumbleniter <tabdeletefinley@gmail.com>
2024-10-29 22:54:32 -0600

    Fixed the load of the database information

--- MongoDBProj/src/lib/utils/database.ts
+++ MongoDBProj/src/lib/utils/database.ts
@@ -1,7 +1,7 @@
 import { MongoClient, Db } from 'mongodb';
 
 let client: MongoClient;
-console.log(import.meta.env.VITE_MONGODB_URI)
+
 const uri = import.meta.env.VITE_MONGODB_URI || process.env.VITE_MONGODB_URI;
 if (!uri) {
     throw new Error('MongoDB URI not defined');
--- MongoDBProj/src/routes/+page.server.ts
+++ MongoDBProj/src/routes/+page.server.ts
@@ -1,7 +1,17 @@
 import { connectToDatabase } from "$lib/utils/database";
+import type { ObjectId } from 'mongodb';
 
 export async function load() {
     const db = await connectToDatabase();
     const items = await db.collection('items').find().toArray();
-    return { items };
- }
+
+    // Convert ObjectId to string
+    const serializedItems = items.map(item => ({
+        ...item,
+        _id: (item._id as ObjectId).toString()
+    }));
+
+    return {
+        items: serializedItems
+    };
+}
